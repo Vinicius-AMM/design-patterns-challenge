c18e73632c7f5b19625e2ffc710abb16308edc92
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-05-20 14:39:36 -0300

    creates the class ClientService,defining the methods coming from the CRUDInterface interface

---
+++ src/main/java/dio/challenge/service/ClientService.java
@@ -0,0 +1,41 @@
+package dio.challenge.service;
+
+import dio.challenge.model.Client;
+import dio.challenge.repository.ClientRepository;
+import dio.challenge.service.interfaces.CRUDInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class ClientService implements CRUDInterface<Client, Long> {
+    @Autowired
+    private ClientRepository clientRepository;
+    @Override
+    public Iterable<Client> findAll() {
+        return clientRepository.findAll();
+    }
+
+    @Override
+    public Client findById(Long id) {
+        Optional<Client> client = clientRepository.findById(id);
+        return client.get();
+    }
+
+    @Override
+    public Client save(Client client) {
+        return clientRepository.save(client);
+    }
+
+    @Override
+    public void update(Long id, Client client) {
+        Optional<Client> clientBd = clientRepository.findById(id);
+        if(clientBd.isPresent()){
+            clientRepository.save(client);
+        }
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        clientRepository.deleteById(id);
+    }
+}
