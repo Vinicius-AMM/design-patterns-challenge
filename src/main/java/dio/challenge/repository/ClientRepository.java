d569e4e3e241f944cb0764b4e914a857b99a6d2c
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-05-20 14:30:46 -0300

    creates the ClientRepository interface

---
+++ src/main/java/dio/challenge/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package dio.challenge.repository;
+
+import dio.challenge.model.Client;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends CrudRepository<Client, Long> {
+}
